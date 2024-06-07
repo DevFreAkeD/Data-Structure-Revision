33fdfd21b3d49b2ec88065ec8cfb9d2c35ca9acd
Tushar Nagar <32740788+DevFreAkeD@users.noreply.github.com>
2024-06-07 21:22:27 +0530

    Create Access_Array_Elements.cpp

---
+++ Arrays/Access_Array_Elements.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    // Array declaration and initialization
+    int arr[5] = { 15, 25, 35, 45, 55 };
+
+    // Accessing element at index 2 i.e. 3rd element
+    std::cout << "Element at arr[2]: " << arr[2] << std::endl;
+
+    // Accessing element at index 4 i.e. last element
+    std::cout << "Element at arr[4]: " << arr[4] << std::endl;
+
+    // Accessing element at index 0 i.e. first element
+    std::cout << "Element at arr[0]: " << arr[0] << std::endl;
+
+    return 0;
+}
